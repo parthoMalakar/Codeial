884fa638cef47a43ce9de271d42b941cb975e458
parthoMalakar <parthmalakar4@gmail.com>
2020-11-29 00:35:00 +0530

    1.Setting current authenticated user 2.Passing User data to views and restrict page access 3.Setting up Mongo store for session cookie

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 
 
 
@@ -27,6 +28,8 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//mongo store is used to store the session cookie in the db
+
 app.use(session({
     name: 'codial',
     //TODO change the secret before deployment in production mode
@@ -35,11 +38,22 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err) {
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 // use express router
 app.use('/', require('./routes/index'));
