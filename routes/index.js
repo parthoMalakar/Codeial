a7248b5bb91d8314024496a7641d207ca4542bd3
parthoMalakar <parthmalakar4@gmail.com>
2020-12-09 13:16:05 +0530

    Saving Posts to the DB

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ console.log('router loaded');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 // for any furthur routes, access from here
 // router.use('/routerName', require('./routerFile'));
